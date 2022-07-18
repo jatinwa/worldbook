2bc9ab79b207df0ca2d31234a41002309820fce2
jatin <jatinwatts8@gmail.com>
2022-07-18 11:46:10 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'www.google.com'
+    ],
+  },
+};
 
 module.exports = nextConfig
